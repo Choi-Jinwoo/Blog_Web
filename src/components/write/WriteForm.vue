c95e31b4ced35e51be7453dd1f190e7a4b0586c5
choi jinwoo <chlwlsdn0828@gmail.com>
2020-03-15 01:06:18 +0900

    [ADD] Write Post File Upload Event(Paste/Drop)

--- src/components/write/WriteForm.vue
+++ src/components/write/WriteForm.vue
@@ -5,12 +5,13 @@
         <input type="text" placeholder="제목을 입력하세요" v-model="post.title" />
       </div>
 
-      <div class="util-bar">
-        <img src="../../assets/svg/write_image_upload.svg" alt />
-      </div>
-
       <div class="content-container">
-        <textarea v-model="post.content" placeholder="내용을 작성해주세요"></textarea>
+        <textarea
+          v-model="post.content"
+          placeholder="내용을 작성해주세요"
+          @drop.prevent="uploadWithDrop"
+          @paste="uploadWithPaste"
+        ></textarea>
       </div>
     </div>
     <div class="result-box">
@@ -19,7 +20,20 @@
     </div>
 
     <div class="write-option">
-      <img src="../../assets/svg/write_upload.svg" />
+      <img
+        class="active-option-btn"
+        src="../../assets/svg/write_upload.svg"
+        v-if="!activeWriteOption"
+        @click="activeWriteOption = true"
+      />
+      <div class="write-option-bar" v-else>
+        임시저장
+        업로드
+        썸네일 업로드
+        공개
+        카테고리
+        취소
+      </div>
     </div>
   </div>
 </template>
@@ -27,12 +41,16 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import marked from "marked";
+import axios, { AxiosResponse } from "axios";
+import getDataFromResp from "@/lib/util/getDataFromResp";
+import { API_ADDR } from "../../../config/server";
 
 type PostType = {
   title: string;
   content: string;
   thumbnail: string;
   category_idx: number;
+  is_private: boolean;
 };
 
 @Component
@@ -41,8 +59,58 @@ export default class Write extends Vue {
     content: ""
   } as PostType;
 
+  activeWriteOption: boolean = false;
+
   get convertedContent() {
     return marked(this.post.content);
+  }
+
+  async uploadWithDrop(e: any) {
+    const reqFiles = e.target.files || e.dataTransfer.files;
+    const formData = new FormData();
+    formData.append("files", reqFiles[0]);
+    this.uploadFiles(formData);
+  }
+
+  async uploadWithPaste(e: any) {
+    const reqFiles = e.clipboardData.files;
+    if (!reqFiles.length) {
+      return;
+    }
+    e.prevent;
+    const formData = new FormData();
+    formData.append("files", reqFiles[0]);
+    this.uploadFiles(formData);
+  }
+
+  async uploadFiles(formData: FormData) {
+    const resp: AxiosResponse = await axios.post(
+      `${API_ADDR}/upload`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      }
+    );
+
+    const { files } = getDataFromResp(resp);
+    files.forEach(async (file: string) => {
+      const url = await this.createURL(file);
+      this.post.content += `![이미지](${encodeURI(url)})\n`;
+    });
+  }
+
+  async createURL(fileName: string): Promise<string> {
+    const resp: AxiosResponse = await axios.post(
+      `${API_ADDR}/file/create-url`,
+      {
+        file_name: fileName
+      }
+    );
+
+    const { url } = getDataFromResp(resp);
+    return url;
   }
 }
 </script>
@@ -74,18 +142,6 @@ export default class Write extends Vue {
         &:focus {
           outline: none;
         }
-      }
-    }
-
-    .util-bar {
-      display: flex;
-      flex-direction: column;
-      align-items: flex-end;
-      justify-content: center;
-      padding-right: 5rem;
-      img {
-        width: 1.75rem;
-        height: 1.75rem;
       }
     }
 
@@ -192,6 +248,16 @@ export default class Write extends Vue {
     img {
       width: 3rem;
       height: 3rem;
+      cursor: pointer;
+    }
+
+    .write-option-bar {
+      display: flex;
+      align-items: center;
+      padding: 0 1rem;
+      height: 3rem;
+      border-radius: 40px;
+      background-color: #ffffff;
     }
   }
 }
