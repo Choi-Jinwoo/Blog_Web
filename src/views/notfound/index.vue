830e8e1ae3c3ae7419657f9e61a440ea527c2d7f
choi jinwoo <chlwlsdn0828@gmail.com>
2020-03-24 21:35:56 +0900

    [UPDATE] NotFound Page Update

--- src/views/notfound/index.vue
+++ src/views/notfound/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="not-found">
-    <h2>아무것도 없어요..</h2>
+    <h1>404</h1>
+    <h2>아무것도 없어요</h2>
     <p @click="$router.push('/')">홈으로</p>
   </div>
 </template>
@@ -12,8 +13,23 @@ export default class NotFound extends Vue {}
 </script>
 
 <style lang="scss" scoped>
+@import "../../style/palette";
+
 .not-found {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   padding: 3rem;
+
+  h1,
+  h2 {
+    margin: 0;
+  }
+
+  h1 {
+    color: $blue3;
+  }
 
   p {
     cursor: pointer;
