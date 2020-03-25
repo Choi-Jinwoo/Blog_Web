ed1cee8830254cad342306f01225455f5530dbb3
choi jinwoo <chlwlsdn0828@gmail.com>
2020-03-25 16:34:59 +0900

    [FIX] Title Bug

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,36 +7,57 @@ const routes = [
   {
     path: '/',
     name: 'Main',
+    meta: {
+      title: 'wlswoo blog'
+    },
     component: () => import('@/views/main/index.vue'),
   },
   {
     path: '/login',
     name: 'Login',
+    meta: {
+      title: 'wlswoo blog - login'
+    },
     component: () => import('@/views/login/index.vue')
   },
   {
     path: '/register',
     name: 'Register',
+    meta: {
+      title: 'wlswoo blog - register'
+    },
     component: () => import('@/views/register/index.vue')
   },
   {
     path: '/write',
     name: 'Write',
+    meta: {
+      title: 'wlswoo blog - write'
+    },
     component: () => import('@/views/write/index.vue'),
   },
   {
     path: '/admin',
     name: 'Admin',
+    meta: {
+      title: 'wlswoo blog - admin'
+    },
     component: () => import('@/views/admin/index.vue'),
   },
   {
     path: '/notice',
     name: 'Notice',
+    meta: {
+      title: 'wlswoo blog - notice'
+    },
     component: () => import('@/views/notice/index.vue'),
   },
   {
     path: '/subscribe',
     name: 'Subscribe',
+    meta: {
+      title: 'wlswoo blog - subscribe'
+    },
     component: () => import('@/views/subscribe/index.vue'),
   },
   {
@@ -55,6 +76,13 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title;
+  }
+  next();
+});
 
 export default router
