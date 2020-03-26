29cfc4b9f8ab47c3e54295801f81cc4b6ea9addf
choi jinwoo <chlwlsdn0828@gmail.com>
2020-03-27 00:22:56 +0900

    [ADD] LocalStrage Token

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,21 @@
   </div>
 </template>
 
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import { Token } from "./lib/Storage";
+
+@Component
+export default class App extends Vue {
+  mounted() {
+    const savedToken: string | null = Token.getSavedToken();
+    if (!Token.getToken() && savedToken) {
+      Token.setToken(savedToken);
+    }
+  }
+}
+</script>
+
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap");
 
--- src/lib/Storage.ts
+++ src/lib/Storage.ts
@@ -7,4 +7,12 @@ export class Token {
   static setToken(token: string) {
     sessionStorage.setItem('x-access-token', token);
   };
+
+  static getSavedToken() {
+    return localStorage.getItem('x-access-token');
+  };
+
+  static setSaedToken(token: string) {
+    localStorage.setItem('x-access-token', token);
+  };
 };
