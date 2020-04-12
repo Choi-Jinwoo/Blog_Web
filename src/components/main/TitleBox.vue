fed0b57fddf23a187615be59b425e743c46b814c
Choi-Jinwoo <49791336+Choi-Jinwoo@users.noreply.github.com>
2020-04-12 23:17:33 +0900

    [ADD] Category Login

--- src/components/main/TitleBox.vue
+++ src/components/main/TitleBox.vue
@@ -4,6 +4,32 @@
       <h1>適者生存</h1>
       <p>적는 자만이 살아남는다</p>
     </div>
+
+    <div class="category-box">
+      <div
+        class="wrapper-category"
+        v-for="(wrapperCategory) in categories"
+        :key="wrapperCategory.idx"
+        @mouseover="showCategoryIdx = wrapperCategory.idx"
+        @mouseleave="showCategoryIdx = null"
+      >
+        <p>{{ wrapperCategory.name }}</p>
+
+        <div
+          class="wrapped-category-box"
+          v-show="showCategoryIdx === wrapperCategory.idx"
+          @mouseleave="showCategoryIdx = null"
+        >
+          <div
+            class="wrapped-category"
+            v-for="(wrappedCategory) in wrapperCategory.categories"
+            :key="wrappedCategory.idx"
+          >
+            <p>{{ wrappedCategory.name }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,7 +49,7 @@ export default class TitleBox extends Vue {
    * categories - 응답 받은 카테고리 배열
    */
   categories: ICategoryResp[] = [];
-  showDetailCategory: boolean = false;
+  showCategoryIdx: number | null = null;
 
   async created() {
     let categories: ICategoryResp[] | undefined = await getCategories();
@@ -51,6 +77,33 @@ export default class TitleBox extends Vue {
 
     h1 {
       font-size: 2.5rem;
+    }
+  }
+
+  .category-box {
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    .wrapper-category {
+      width: 5rem;
+      font-size: 1rem;
+      color: $gray6;
+      cursor: pointer;
+
+      &:hover {
+        color: $gray3;
+      }
+
+      .wrapped-category-box {
+        padding: 1rem;
+
+        .wrapped-category {
+          font-size: 0.75rem;
+          color: $gray6;
+          border-right: 1px solid $gray6;
+          border-left: 1px solid $gray6;
+        }
+      }
     }
   }
 }
